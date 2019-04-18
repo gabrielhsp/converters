d41a11ec8dbb6eb7d57f7db90decad6642e1440f
Gabriel Henrique <gabhenrique0@gmail.com>
2019-04-18 11:08:48 -0300

    Finish implementation of calculate methods inside ViewController and add constraints inside layout on Main.storyboard

--- Converters/Converters/ViewController.swift
+++ Converters/Converters/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonUnitTwo: UIButton!
     @IBOutlet weak var labelResult: UILabel!
     @IBOutlet weak var labelResultUnit: UILabel!
+    @IBOutlet weak var labelUnit: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +22,107 @@ class ViewController: UIViewController {
     
     
     @IBAction func actionShowNext(_ sender: UIButton) {
+        switch labelUnit.text! {
+            case "Temperatura":
+                labelUnit.text = "Peso"
+                buttonUnitOne.setTitle("Kilograma", for: .normal)
+                buttonUnitTwo.setTitle("Libra", for: .normal)
+            
+            case "Peso":
+                labelUnit.text = "Moeda"
+                buttonUnitOne.setTitle("Real", for: .normal)
+                buttonUnitTwo.setTitle("Dólar", for: .normal)
+            
+            case "Moeda":
+                labelUnit.text = "Distância"
+                buttonUnitOne.setTitle("Metro", for: .normal)
+                buttonUnitTwo.setTitle("Kilômetro", for: .normal)
+            
+            default:
+                labelUnit.text = "Temperatura"
+                buttonUnitOne.setTitle("Celsius", for: .normal)
+                buttonUnitTwo.setTitle("Fahrenheit", for: .normal)
+        }
+        
+        actionConvert(nil)
     }
     
-    @IBAction func actionConvert(_ sender: UIButton) {
+    @IBAction func actionConvert(_ sender: UIButton?) {
+        if let sender = sender {
+            if sender == buttonUnitOne {
+                buttonUnitTwo.alpha = 0.5
+            } else {
+                buttonUnitOne.alpha = 0.5
+            }
+            
+            sender.alpha = 1.0
+        }
+        
+        switch labelUnit.text! {
+            case "Temperatura":
+                calculateTemperature()
+            
+            case "Peso":
+                calculateWeight()
+            
+            case "Moeda":
+                calculateCurrency()
+        
+            default:
+                calculateDistance()
+        }
+        
+        view.endEditing(true)
+        
+        let result = Double(labelResult.text!)!
+        labelResult.text = String(format: "%.2f", result)
+    }
+    
+    func calculateTemperature() {
+        guard let temperature = Double(textFieldValue.text!) else { return }
+        
+        if buttonUnitOne.alpha == 1.0 {
+            labelResultUnit.text = "Fahrenheit"
+            labelResult.text = String(temperature * 1.8 + 32.0)
+        } else {
+            labelResultUnit.text = "Celsius"
+            labelResult.text = String((temperature - 32.0) / 1.8)
+        }
+    }
+    
+    func calculateWeight() {
+        guard let weight = Double(textFieldValue.text!) else { return }
+        
+        if buttonUnitOne.alpha == 1.0 {
+            labelResultUnit.text = "Libra"
+            labelResult.text = String(weight / 2.2046)
+        } else {
+            labelResultUnit.text = "Kilograma"
+            labelResult.text = String(weight * 2.2046)
+        }
+    }
+    
+    func calculateCurrency() {
+        guard let currency = Double(textFieldValue.text!) else { return }
+        
+        if buttonUnitOne.alpha == 1.0 {
+            labelResultUnit.text = "Dólar"
+            labelResult.text = String(currency / 3.5)
+        } else {
+            labelResultUnit.text = "Real"
+            labelResult.text = String(currency * 3.5)
+        }
+    }
+    
+    func calculateDistance() {
+        guard let distance = Double(textFieldValue.text!) else { return }
+        
+        if buttonUnitOne.alpha == 1.0 {
+            labelResultUnit.text = "Kilômetro"
+            labelResult.text = String(distance / 1000.0)
+        } else {
+            labelResultUnit.text = "Metros"
+            labelResult.text = String(distance * 1000.0)
+        }
     }
 }
